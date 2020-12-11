e0bf0327fec448d50ce1c51c69f106b8b84b4eb7
Matheus <matheussouzaslv2@gmail.com>
2020-12-10 22:02:40 -0300

    Projeto Estruturado

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+//const routes = require('./routes/index')
+
+
+const app = express()
+const port = 8000
+
+app.use(express.json());
+
+//routes(app);
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em ${port}`)
+})
+
+module.exports = app
